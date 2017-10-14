5d7a8033d84819ef313fbe59a0589fccc59ca944
Will Hodge <will.hodge18@gmail.com>
2017-10-13 20:24:46 -0400

    adding stuff

--- spotify/src/components/App.js
+++ spotify/src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Brady from '../images/sex.jpg';
+import Dropdown from './Dropdown';
 
 class App extends React.Component {
   render() {
     return (
+      <div>
+      <Dropdown />
       <div className="App">
         <header className="App-header">
           <img src={Brady} className="App-logo" alt="logo" />
           <h1 className="App-title">Time for Sex</h1>
         </header>
+      </div>
       </div>
     );
   }
---
+++ spotify/src/components/Dropdown.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ToolboxDropdown from 'react-toolbox/lib/button/Dropdown';
+
+class Dropdown extends ToolboxDropdown {
+
+}
+
+export default Dropdown;
