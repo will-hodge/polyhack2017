70efb4d08173e5347aeb450211e8dc891bd31409
Will Hodge <will.hodge18@gmail.com>
2017-10-13 21:29:15 -0400

    fixing?

--- spotify/server.js
+++ spotify/server.js
@@ -2,6 +2,21 @@ import express from 'express';
 import request from 'request';
 import querystring from 'querystring';
 import cookieParser from 'cookie-parser';
+import config from 'config';
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.render('index', {
+  });
+});
+
+
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+  console.info('Express listening on port ', config.port);
+});
 
 let client_id = 'CLIENT_ID'; // Your client id
 let client_secret = 'CLIENT_SECRET'; // Your secret
